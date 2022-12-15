29425b849e56c6d0a2ef62547a048a4b6695215b
Yuqiu Yang <yyqengels@gmail.com>
2022-12-14 21:09:21 -0600

    Docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -3,6 +3,10 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
+import os 
+import sys
+sys.path.insert(0, os.path.abspath('../../'))
+
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
@@ -14,7 +18,17 @@ release = '0.0.1'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    # A theme
+    'sphinx_rtd_theme',
+    # For automatic doc generation
+    'sphinx.ext.autodoc',
+    # Just some info
+    'sphinx.ext.duration',
+    'sphinx.ext.doctest',
+    'sphinx.ext.coverage',
+    # For Google and Numpy docstring
+    'sphinx.ext.napoleon',]
 
 templates_path = ['_templates']
 exclude_patterns = []
@@ -24,5 +38,6 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
+html_logo = "../../assets/logo.png"
 html_static_path = ['_static']
